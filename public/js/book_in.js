fc1ba06a9f38d0241a9eeac796dfc588fa0642f3
booldook <booldook@gmail.com>
2019-05-20 12:04:16 +0900

    book_in.html

--- public/js/book_in.js
+++ public/js/book_in.js
@@ -1,1 +1,54 @@
 $("#wdate").datepicker();
+$("#bt_save").click(function(){
+	var $f = $(document.book_in);
+	var title = $("input[name='title']", $f);
+	var author = $("input[name='author']", $f);
+	var price = $("input[name='price']", $f);
+	var isbn = [];
+	isbn.push($("input[name='isbn_0']", $f));
+	isbn.push($("input[name='isbn_1']", $f));
+	isbn.push($("input[name='isbn_2']", $f));
+	var wdate = $("input[name='wdate']", $f);
+	var cnt = $("input[name='cnt']", $f);
+	var summary = $("textarea[name='summary']", $f);
+
+	if(title.val() == "") {
+		alert("책 제목을 입력하세요.");
+		return false;
+	}
+	if(author.val() == "") {
+		alert("저자를 입력하세요.");
+		author.focus();
+		return false;
+	}
+	if(price.val() == "") {
+		alert("판매금액을 입력하세요.");
+		price.focus();
+		return false;
+	}
+	if(isbn[0].val() == "" && isbn[0].length < 2) {
+		alert("ISBN을 입력하세요. 두자리 입니다.");
+		isbn[0].focus();
+		return false;
+	}
+	if(isbn[1].val() == "" && isbn[0].length < 4) {
+		alert("ISBN을 입력하세요. 네자리 입니다.");
+		isbn[1].focus();
+		return false;
+	}
+	if(isbn[2].val() == "" && isbn[0].length < 4) {
+		alert("ISBN을 입력하세요. 네자리 입니다.");
+		isbn[2].focus();
+		return false;
+	}
+	if(wdate.val() == "") {
+		alert("발행일을 입력하세요.");
+		wdate.focus();
+		return false;
+	}
+	if(summary.val() == "") {
+		alert("요약정보를 입력하세요.");
+		summary.focus();
+		return false;
+	}
+});
